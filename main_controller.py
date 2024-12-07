9b9799404ec3fd30cfb8f9aeee7cc562a31c47ae
Aaaaaaaaaaaaaaaaaaaaron <aaron.lange@spontan.org>
2024-12-07 23:40:34 +0100

    feat[wifi]: add data transfer over wifi
    interepreted analog joystick data and sends via wifi to car

--- main_controller.py
+++ main_controller.py
@@ -1,14 +1,30 @@
 from machine import Pin, PWM, ADC
+import network
+import espnow
 import time
- 
-# Pinbelegung für den Joystick
-VRX_PIN = 12  # GPIO39 (ADC3) für VRX
-VRY_PIN = 14  # GPIO36 (ADC0) für VRY
-SW_PIN = 27   # GPIO17 für den Button
+from machine import Pin
+import sys
+import select
+# Konfiguration der Wi-Fi-Verbindung im station mode
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)  # Wi-Fi aktivieren
+wlan.disconnect()  # Trennen, da wir ESP-NOW verwenden werden
 
-VRX2_PIN = 26  # 
-VRY2_PIN = 25  # 
-SW2_PIN = 33   # nicht aktiviert
+# Initialisierung von ESP-NOW
+e = espnow.ESPNow()
+e.active(True)  # ESP-NOW aktivieren
+
+# MAC-Adresse des Empfängers (Auto)
+peer = b'\x1c\x69\x20\xcd\x31\xbc'  # car mac: 1c:69:20:cd:31:bc
+e.add_peer(peer)
+# Pinbelegung für den Joystick
+VRX_PIN = 33  # GPIO39 (ADC3) für VRX
+VRY_PIN = 32  # GPIO36 (ADC0) für VRY
+SW_PIN = 35   # GPIO17 für den Button
+
+VRX2_PIN = 34  # 
+VRY2_PIN = 39  # 
+SW2_PIN = 36   # nicht aktiviert
 
 # Initialisierung der Joystick-Pins
 vrx = ADC(Pin(VRX_PIN))
@@ -25,6 +41,11 @@ vry2.atten(ADC.ATTN_11DB)
 # Schwellenwert für Joystick
 JOYSTICK_THRESHOLD = 1750
 JOYSTICK_THRESHOLD2 = 1750
+
+
+
+# Variable zur Steuerung der Befehlssendung
+command_sent = None
  
 # Hauptloop
 while True:
@@ -41,19 +62,48 @@ while True:
     # Bedingung: Joystick-Werte über dem Schwellenwert
     if x_value >= JOYSTICK_THRESHOLD or x_value <= JOYSTICK_THRESHOLD:
         # Bedingung: Joystick ganz nach vorne gedrückt
-        if x_value > 1850:  # Schwellenwert für "nach vorne" (anpassen, falls nötig)
-            print("motor1 vorwärts.")
+        if x_value > 2050 and x_value2 > 1050 and x_value2 < 2050:  # Schwellenwert für "nach vorne" (anpassen, falls nötig)
+            print("motor1 vorwärts.") #right
+            e.send(peer, b'm1v')
+            print("Command 'm1v' sent")
+            command_sent = 'm1v'
             
-        elif x_value < 1650:
-            print("motor1 rückwärts")
+        if x_value < 1050 and x_value2 > 1050 and x_value2 < 2050:
+            print("motor1 rückwärts") #left
+            e.send(peer, b'm1r')
+            print("Command 'm1r' sent")
+            command_sent = 'm1r'
         
-        if x_value2 > 1850:  # Schwellenwert für "nach vorne" (anpassen, falls nötig)
-            print("motor2 vorwärts.")
+        if x_value2 > 2050 and x_value > 1050 and x_value < 2050:  # Schwellenwert für "nach vorne" (anpassen, falls nötig)
+            print("motor2 vorwärts.") #left
+            e.send(peer, b'm2v')
+            print("Command 'm2v' sent")
+            command_sent = 'm2v'
             
-        elif x_value2 < 1650:
-            print("motor2 rückwärts")
-        else:
-            print("Auto steht.")
+        if x_value2 < 1050 and x_value > 1050 and x_value < 2050:
+            print("motor2 rückwärts") #right
+            e.send(peer, b'm2r')
+            print("Command 'm2r' sent")
+            command_sent = 'm2r'
+            
+        if x_value2 < 1050 and x_value < 1050:
+            print("backward")
+            e.send(peer, b'backward')
+            print("Command 'backward' sent")
+            command_sent = 'backward'
+            
+        if x_value2 > 2050 and x_value > 2050:
+            print("forward")
+            e.send(peer, b'forward')
+            print("Command 'forward' sent")
+            command_sent = 'forward'
+        
+        if x_value > 1050 and x_value < 2050 and x_value2 > 1050 and x_value2 < 2050: 
+            print("Auto steht.") #stop
+        
+            e.send(peer, b'stop')
+            print("Command 'stop' sent")
+            command_sent = 'stop'
             
 
     else:
@@ -61,4 +111,3 @@ while True:
        
  
     time.sleep(0.1)  # Kleines Delay für Stabilität
-
